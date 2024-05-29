006e479ca5ca8a0e6b527a3c36eee8512f2d2033
prajein <prajeinck@gmail.com>
2024-05-29 17:30:07 +0530

    2nd commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,10 @@
 import './About.css'
 import React from "react";
-import logo from '../../assets/logo.png'
+import logo from '../../assets/images/hmgtext.png';
 import { textVariant, slideIn } from '../../utils/motion';
 import { motion } from 'framer-motion'
 import sectionWrapper from '../../utils/sectionWrapper'
-import Logo from '../../canvas/Logo';
+import Logo from '../Logo/Logo';
 
 
 const About = () => {
@@ -13,22 +13,18 @@ const About = () => {
             <div className='about__container'>
 
                 <div className='about__card'>
-                    {/* <motion.div variants={slideIn("left", "ease", 0, 1)} className='about__card__img__container'>
-                     
-                    </motion.div> */}
                     <div className='about__card__img__container'>
-                        <Logo />
+                    <Logo/>
                     </div>
-
+                 
                     <div className='about__card__sub'>
-                        <img src={logo} className='about__card__img' />
-                        {/* <motion.h3 className='about__card__title' variants={textVariant()}>INNOVENTZZ'24</motion.h3> */}
+                    <img src={logo} className='about__card__img' />
                         <motion.p className='about__card__para' variants={textVariant()}>
-                            Welcome to <span className='about__card__bold'>Innoventzz</span>, an intellectually stimulating symposium hosted by the  <span className='about__card__bold'>Department of Artificial Intelligence and Machine Learning </span> dynamically together with the <span className='about__card__bold'> Department of Artificial Intelligence and Data Science </span>. Innoventzz is more than just a gathering; it's a celebration of the trailblazing advancements and limitless possibilities within the realms of AI, Machine Learning and Data Science. You are at the right place to flaunt your innovations and creativity. Gear up yourselves for a thought - <span className='about__card__bold'>provoking experience from INNOVENTZZ’24. </span>
+                            Welcome to <span className='about__card__bold'>HACKMAGEDDON</span>, an intellectually stimulating symposium hosted by the  <span className='about__card__bold'>Department of Artificial Intelligence and Machine Learning </span> dynamically together with the <span className='about__card__bold'> Department of Artificial Intelligence and Data Science </span>. Innoventzz is more than just a gathering; it's a celebration of the trailblazing advancements and limitless possibilities within the realms of AI, Machine Learning and Data Science. You are at the right place to flaunt your innovations and creativity. Gear up yourselves for a thought - <span className='about__card__bold'>provoking experience from INNOVENTZZ’24. </span>
                             <br></br>
                             <br></br>
-                            <motion.h3 className='about__card__title' variants={textVariant()}>About Us</motion.h3>
-                            The department of Artificial Intelligence and Machine Learning was established at <span className='about__card__bold'>Rajalakshmi Engineering College </span> in the year 2020. The growing demand for these technologies gave rise to the Department of Artificial Intelligence and Data Science in the year 2022. These emerging departments have proved that actions speak louder than words by bringing home tremendous achievements within a short span. The main motive of Rajalakshmi Engineering College is to encourage young minds in these evolving technologies. The esteemed <span className='about__card__bold'>Head of the Department Dr. K. Sekar </span> along with the enthusiastic faculty members are the support system of these budding departments.
+                            {/* <motion.h3 className='about__card__title' variants={textVariant()}>About Us</motion.h3>
+                            The department of Artificial Intelligence and Machine Learning was established at <span className='about__card__bold'>Rajalakshmi Engineering College </span> in the year 2020. The growing demand for these technologies gave rise to the Department of Artificial Intelligence and Data Science in the year 2022. These emerging departments have proved that actions speak louder than words by bringing home tremendous achievements within a short span. The main motive of Rajalakshmi Engineering College is to encourage young minds in these evolving technologies. The esteemed <span className='about__card__bold'>Head of the Department Dr. K. Sekar </span> along with the enthusiastic faculty members are the support system of these budding departments. */}
                         </motion.p>
                     </div>
 
